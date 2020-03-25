a58659c872f08c7b582cf38d95be31ea67e8f3c7
ridlees <45198686+ridlees@users.noreply.github.com>
2020-03-25 22:07:05 +0100

    init

---
+++ teta.py
@@ -0,0 +1,92 @@
+'''
+Moje teta - tvoje teta
+pravidla:   32 karet (7,8,9,10,J,Q,K,A)
+            zamíchám
+            ukazuji poslední kartu z balíku
+            hráč si vybere dvě pole, na jedno vsadí, na druhé ne
+            padne-li typ (neřeším barvu, ale vizuálně jo), vyhrává za každou co tam padne
+        
+'''
+def End(positions):
+     for i in range (8):
+          le = len(positions.positions[i])
+          if le > 4:
+               player = positions.positions[0][0][0]
+               value = positions.positions[0][0][1]
+               amount = positions.positions[0][0][2]
+               for z in range (1,le):
+                    if positions.positions[i][z].numb == str(value):
+                         print(f"{player} wins {amount}")
+           
+     
+def Play(p,positions,deck):
+     for i in range (32):
+          positions.play(p,deck.cards[0])
+          deck.remove()
+          p = p+1
+          if p == 8:
+               p = 0
+     End(positions)
+     
+class Positions:
+     
+     def __init__(self):
+          
+          self.positions = []
+          for i in range(0,8):
+               self.positions.append([])
+          
+     def bet(self, p1,v1,amount,p2):
+          
+          self.positions[p1].append(["Player",v1,amount])
+          self.positions[p2].append(["Dealer",v1,amount])
+          
+     def play(self,p,card):
+          
+          self.positions[p].append(card)
+    
+class Card:
+    
+     def __init__(self,numb,color):
+          
+         self.numb = numb
+         self.color = color
+         
+class Deck:
+
+     def shuffle(self):
+          
+        import random
+        random.shuffle(self.cards)
+        
+     def last(self):
+          
+          return self.cards[-1]
+    
+     def __init__(self):
+          
+          self.cards = []
+          cards = ["7","8","9","10","J","Q","K","A"]
+          for card in cards:
+               self.cards.append(Card(card,0))
+               self.cards.append(Card(card,1))
+               self.cards.append(Card(card,2))
+               self.cards.append(Card(card,3))
+
+     
+     
+     def remove(self):
+          self.cards = self.cards[1:]       
+          
+        
+if __name__ == '__main__':
+     deck = Deck()
+     deck.shuffle()
+     deck.shuffle()
+     deck.shuffle()
+     positions = Positions()
+     print(deck.last().numb,deck.last().color)
+     positions.bet(0,"K",100,1)
+     positions.bet(2,"K",100,3)
+     positions.bet(4,"K",100,5)
+     positions.bet(6,"K",100,7)
